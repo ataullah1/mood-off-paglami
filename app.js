edb6bc41a8d80fc93c1f6b71266086af7b7ef305
Md Ataullah <ataullahm100@gmail.com>
2023-12-20 14:08:54 +0600

    Paglami Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// function starFunc() {
+//   let str = document.querySelector('#star');
+//   let roted = 0;
+//   let intervel = setInterval(rotedTime, 5);
+//   function rotedTime() {
+//     roted++;
+//     str.style.rotate = 360 + 'deg';
+//   }
+// }
+// starFunc();
+
+function btnPaglamiF() {
+  let a = document.getElementById('paglami');
+  a.style.display = 'block';
+}
+function btnPaglamiF() {
+  let a = document.getElementById('paglami');
+  a.style.display = 'block';
+}
